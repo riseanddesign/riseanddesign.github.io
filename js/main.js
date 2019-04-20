bffc0eb284017f717e4d8ab041545cd1f0a7988e
Payalben Patel <pate0564@algonquinlive.com>
2019-04-19 20:04:00 -0400

    Cleaning up all pages extra css and added back to top

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
 
+    // Call to AOS plugin
+    AOS.init(); 
+
+    // Back to top JS
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+    
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
+      
+
+});
